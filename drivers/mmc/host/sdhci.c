08842e5f1a65dbc071028755ec1ba4baa67b7ec4
imoseyon <imoseyon@gmail.com>
2015-01-27 20:52:05 -0800

    Linux 3.10.66

--- drivers/mmc/host/sdhci.c
+++ drivers/mmc/host/sdhci.c
@@ -1722,6 +1722,8 @@ static void sdhci_request(struct mmc_host *mmc, struct mmc_request *mrq)
 					SDHCI_CARD_PRESENT;
 	}
 
+	present = mmc_gpio_get_cd(host->mmc);
+
 	spin_lock_irqsave(&host->lock, flags);
 
 	WARN_ON(host->mrq != NULL);
@@ -1753,7 +1755,7 @@ static void sdhci_request(struct mmc_host *mmc, struct mmc_request *mrq)
 		host->mrq->cmd->error = -ENOMEDIUM;
 		tasklet_schedule(&host->finish_tasklet);
 	} else {
-		u32 present_state;
+u32 present_state;
 
 		present_state = sdhci_readl(host, SDHCI_PRESENT_STATE);
 		/*
