f5c4588753ad011f77139bc976d641c2b8ed2a49
ZIM <zim555@mail.ru>
2016-09-13 20:36:17 +0300

    mm: swap: don't delay swap free for fast swap devices

--- include/linux/swap.h
+++ include/linux/swap.h
@@ -155,7 +155,7 @@ enum {
 					/* add others here before... */
 	SWP_SCANNING	= (1 << 8),	/* refcount in scan_swap_map */
 	SWP_FAST	= (1 << 9),	/* blkdev access is fast and cheap */
-};
+
 
 #define SWAP_CLUSTER_MAX 32UL
 #define COMPACT_CLUSTER_MAX SWAP_CLUSTER_MAX
